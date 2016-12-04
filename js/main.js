7b2250837d96fd7f0cbf1e7926b014c7ae9e7197
123prove123 <123prove123@gmail.com>
2016-12-04 14:51:31 +0600

    create new

--- js/main.js
+++ js/main.js
@@ -1,40 +1,9 @@
+$(document).ready(function () {
+    initMaterialize();
+});
 
 
-
-function eventInit() {
-
-    (function () {
-
-        const $popup = $('.aside-item-generate');
-        const $openPopup = $('.content .aside .aside-content .aside-item .aside-item_link');
-        const $closePopup = $('.aside-item_close');
-
-        $closePopup.click(function () {
-            $popup.css({'left': '-100%'});
-        });
-
-        $openPopup.click(function () {
-            $popup.css({'left': '0%'});
-        });
-    }());
-
-    // end
-    (function () {
-        const $popup = $('.guide-item-popup');
-        const $openPopup = $('.guide-item a');
-        const $closePopup = $('.popup-close');
-
-        $closePopup.click(function () {
-            $popup.css({'top': '-100%'});
-        });
-
-        $openPopup.click(function () {
-            $popup.css({'top': '0%'});
-        });
-    }());
-
-
-
-
+function initMaterialize() {
+    $('select').material_select();
+    $('.modal').modal();
 }
-eventInit();
